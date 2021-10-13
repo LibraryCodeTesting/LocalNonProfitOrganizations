07141dc3171a87d207d4b0e38e13c70289fe4d92
LibraryCodeTesting <91985909+LibraryCodeTesting@users.noreply.github.com>
2021-10-13 10:13:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -47,8 +47,6 @@ $('#filter4').keyup(function() {
 
 *************************************************/
 
-
-
 function filterTable() {
   // Variables
   let dropdown, table, rows, cells, country, filter;
@@ -60,9 +58,9 @@ function filterTable() {
   // Loops through rows and hides those with countries that don't match the filter
   for (let row of rows) { // `for...of` loops through the NodeList
     cells = row.getElementsByTagName("td");
-    country = cells[1] || null; // gets the 2nd `td` or nothing
+    special = cells[1] || null; // gets the 2nd `td` or nothing
     // if the filter is set to 'All', or this is the header row, or 2nd `td` text matches filter
-    if (filter === "All" || !country || (filter === country.textContent)) {
+    if (filter === "All" || !special || (special.textContent.includes(filter))) {
       row.style.display = ""; // shows this row
     }
     else {
@@ -71,4 +69,24 @@ function filterTable() {
   }
 }
 
-/* remove country references ************************/
+function filterTable2() {
+  // Variables
+  let dropdown, table, rows, cells, country, filter;
+  dropdown = document.getElementById("citiesDropdown");
+  table = document.getElementById("myTable");
+  rows = table.getElementsByTagName("tr");
+  filter = dropdown.value;
+
+  // Loops through rows and hides those with countries that don't match the filter
+  for (let row of rows) { // `for...of` loops through the NodeList
+    cells = row.getElementsByTagName("td");
+    special = cells[3] || null; // gets the 2nd `td` or nothing
+    // if the filter is set to 'All', or this is the header row, or 2nd `td` text matches filter
+    if (filter === "All" || !special || (special.textContent.includes(filter))) {
+      row.style.display = ""; // shows this row
+    }
+    else {
+      row.style.display = "none"; // hides this row
+    }
+  }
+}
